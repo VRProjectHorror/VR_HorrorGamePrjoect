5aa512618058e4d6e048e8137ebad12d641ef437
HyunMin Lee <hmlee4135@naver.com>
2022-06-25 21:58:00 +0900

    플레이어 그랩

--- VR_HorrorGame_Proj/Assets/HM/4. Scripts/Player_Grabable.cs
+++ VR_HorrorGame_Proj/Assets/HM/4. Scripts/Player_Grabable.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Grabable : MonoBehaviour
 {
+    public Transform grabpos;
+    GameObject grabObj;
+
+    bool isGrab = false;
+    bool isContact = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,45 @@ public class Player_Grabable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GrabObj();
+        print(grabObj);
+    }
+
+    private void GrabObj()
+    {
+        if(isContact == true && isGrab == false && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch) == true)
+        {
+            grabObj.transform.SetParent(grabpos);
+
+            isGrab = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("GrabPos") == true && isGrab == false)
+        {
+
+            print("TriggerEnter");
+            isContact = true;
+            grabObj = other.gameObject;
+
+            if(isGrab == false)
+            {
+                OVRInput.SetControllerVibration(0.1f, 0.1f, OVRInput.Controller.RTouch);
+
+                
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("GrabPos") ==true && isGrab == false)
+        {
+            print("TriggerExit");
+            isContact = false;
+            grabObj = null;
+        }
     }
 }
