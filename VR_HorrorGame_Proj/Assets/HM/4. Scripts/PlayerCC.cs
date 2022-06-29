f62702e652bf71ea6274357e533117322a07f9cb
HyunMin Lee <hmlee4135@naver.com>
2022-06-29 14:19:53 +0900

    최종수정

--- VR_HorrorGame_Proj/Assets/HM/4. Scripts/PlayerCC.cs
+++ VR_HorrorGame_Proj/Assets/HM/4. Scripts/PlayerCC.cs
@@ -86,7 +86,7 @@ public class PlayerCC : MonoBehaviour
         RaycastHit hit;
 
         int playerLayer = 1 << LayerMask.NameToLayer("Player");
-        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, 15, ~playerLayer))
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, 5, ~playerLayer))
         {
 
 
